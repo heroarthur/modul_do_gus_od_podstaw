074e83c5575a0b69027bd5462036d57cbe84c7b2
tora wareta <19kkarol96@gmail.com>
2018-07-12 19:05:53 +0200

    Dodaj pliki projektów.

---
+++ modul_do_gus/Module1.vb
@@ -0,0 +1,7 @@
+﻿Module Gus_laczenie
+
+    Sub Main()
+
+    End Sub
+
+End Module
---
+++ modul_do_gus/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+' zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' powiązane z zestawem.
+
+' Sprawdź wartości atrybutów zestawu
+
+<Assembly: AssemblyTitle("modul_do_gus")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("modul_do_gus")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+<Assembly: Guid("3f4fe882-63e7-49c5-b41d-3ca14a5f7f32")>
+
+' Informacje o wersji zestawu zawierają następujące cztery wartości:
+'
+'      Wersja główna
+'      Wersja pomocnicza
+'      Numer kompilacji
+'      Rewizja
+'
+' Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+' przy użyciu symbolu „*”, tak jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
